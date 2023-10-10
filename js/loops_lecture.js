adc5da522b4409655fbf2ef24e75775af36489cb
Lauren Gates <laurengates@Laurens-Air.localdomain>
2023-10-10 14:52:45 -0500

    loops lecture notes

---
+++ js/loops_lecture.js
@@ -0,0 +1,80 @@
+console.log("loops!");
+
+//FOR LOOP
+//**do syntax first for() { }, then fill in
+
+// for(/*initializer*/ ; /*condition*/ ; /*increments*/) {
+//     /* the code tht will repeat */
+// }
+
+
+// for(let i = 0; i <= 5; i++) {
+//     console.log(`The code ran at index ${i}`);
+// }
+
+
+//WHILE LOOP
+
+/*
+while(condition) {
+
+}
+*/
+
+// let index = 0;
+// while(index < 10) {
+//     console.log(`The code ran at index ${index}`);
+//     index++;
+// }
+//
+// let favPizza = prompt(`What is your favorite pizza?`);
+// while(favPizza !== "pepperoni") {
+//     favPizza = prompt(`WRONG ANSWER! TRY AGAIN!`);
+// }
+// alert(`That's what I thought. That's a great choice!`);
+
+
+//DO-WHILE LOOP
+
+/*
+do {
+    //body
+} while(condition)
+
+*/
+
+// let myNumber = 0;
+//
+// do {
+//     console.log(myNumber);
+//     myNumber++;
+// } while( myNumber <= 10);
+
+// const buildRow = (num) => {
+//     let result = "";
+//     for(let i = 0; i < num; i++) {
+//         result += "*";
+//     }
+//     result = "\n";
+//     return result;
+// };
+//
+// const buildPyramid = (rows) => {
+//     let result = "";
+//     //first for loop
+//         for (let i = 1; i <= rows; i++) {
+//             result += buildRow(i);
+//             //result += "*".repeat(i);
+//         }
+//     return result;
+// };
+//
+// let pyramid = buildPyramid(5);
+// console.log(pyramid);
+
+for (let i = 0; i <= 10; i++) {
+    if(i === 5) {
+        continue; //skips the iteration while 'break' will end it
+    }
+    console.log(`The current index is ${i}`);
+}
