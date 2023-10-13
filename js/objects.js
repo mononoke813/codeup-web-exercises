0ca6392f2094511b921589d8c467f36028cc551e
mononoke813 <mononoke813@hotmail.com>
2023-10-13 11:26:07 -0500

    finished objects exercises, but not bonus

--- js/objects.js
+++ js/objects.js
@@ -11,17 +11,17 @@
      *  > console.log(person.firstName) // "Rick"
      *  > console.log(person.lastName) // "Sanchez"
      */
-
-    const person = {
-        firstName: "Lauren",
-        lastName: "Gates",
-        sayHello: function(){
-            const message = `Hello from ${(this.firstName)} ${(this.lastName)}!`;
-            return message;
-        }
-    };
-    console.log(person.firstName);
-    console.log(person.lastName);
+    //
+    // const person = {
+    //     firstName: "Lauren",
+    //     lastName: "Gates",
+    //     sayHello: function(){
+    //         const message = `Hello from ${(this.firstName)} ${(this.lastName)}!`;
+    //         return message;
+    //     }
+    // };
+    // console.log(person.firstName);
+    // console.log(person.lastName);
 
     /**
      * TODO:
@@ -33,7 +33,7 @@
      * > console.log(person.sayHello()) // "Hello from Rick Sanchez!"
      */
 
-    console.log(person.sayHello());
+    //console.log(person.sayHello());
 
 
     /** TODO:
@@ -50,23 +50,23 @@
      * and console.log the relevant messages for each person
      */
 
-    const shoppers = [
-        {name: 'Cameron', amount: 180},
-        {name: 'Ryan', amount: 250},
-        {name: 'George', amount: 320}
-    ];
-
-    shoppers.forEach((shopper) => {
-        const {name, amount} = shopper;
-            if(amount >= 200) {
-                const savingTotal = (amount*.12).toLocaleString("en-US", {style:"currency", currency: "USD"});
-                const discount = (amount * (1 - .12)).toLocaleString("en-US", {style:"currency", currency: "USD"});
-                console.log(`${name}, your bill is ${amount.toLocaleString("en-US", {style:"currency", currency: "USD"})}, but with a 12% discount you save ${savingTotal} and only need to pay ${discount}!`);
-            } else {
-                console.log(`${name}, you do not get a discount.`);
-            }
-
-        });
+    // const shoppers = [
+    //     {name: 'Cameron', amount: 180},
+    //     {name: 'Ryan', amount: 250},
+    //     {name: 'George', amount: 320}
+    // ];
+    //
+    // shoppers.forEach((shopper) => {
+    //     const {name, amount} = shopper;
+    //         if(amount >= 200) {
+    //             const savingTotal = (amount*.12).toLocaleString("en-US", {style:"currency", currency: "USD"});
+    //             const discount = (amount * (1 - .12)).toLocaleString("en-US", {style:"currency", currency: "USD"});
+    //             console.log(`${name}, your bill is ${amount.toLocaleString("en-US", {style:"currency", currency: "USD"})}, but with a 12% discount you save ${savingTotal} and only need to pay ${discount}!`);
+    //         } else {
+    //             console.log(`${name}, you do not get a discount.`);
+    //         }
+    //
+    //     });
 
 
     /** TODO:
@@ -82,19 +82,47 @@
      * > console.log(books[0].author.lastName) // "Adams"
      */
     const books = [
+
         {
             title: "Fairy Animals of Misty Wood",
             author: {
                 firstName: "Lily",
-                lastName: "Small"
-            }
+                lastName: "Small",
+            },
         },
-        {title: "", author: {firstName: , lastName:}},
-        {title: , author: {firstName: , lastName:}}
-        {title: , author: {firstName: , lastName:}},
-        {title: , author: {firstName: , lastName:}}
+        {
+            title: "The Perfect Police State",
+            author: {
+                firstName: "Geoffrey",
+                lastName: "Cain",
+            },
+        },
+        {
+            title:"The Jungle Book",
+            author: {
+                firstName: "Rudyard",
+                lastName: "Kipling",
+            },
+        },
+        {
+            title: "The Tales of Beedle the Bard",
+            author: {
+                firstName: "J.K.",
+                lastName: "Rowling",
+            },
+        },
+        {
+            title: "On the Origin of Species by Means of Natural Selection",
+            author: {
+                firstName: "Charles",
+                lastName: "Darwin",
+            },
+        },
     ];
 
+    // console.log(books[4].title);
+    // console.log(books[4].author.firstName);
+    // console.log(books[4].author.lastName);
 
 
     /**
@@ -121,6 +149,11 @@
      *      ---
      *      ...
      */
+    // books.forEach((book, index) => {
+    //     const bookNum = index + 1;
+    //     const { title, author } = book;
+    //     console.log(`Book ${bookNum}: \nTitle: ${title} \n Author: ${author.firstName} ${author.lastName}`);
+    // });
 
     /**
      * Bonus:
@@ -132,5 +165,24 @@
      *   outputs the information described above. Refactor your loop to use your
      *   `showBookInfo` function.
      */
+
+    const createBook = (title, authorFirstName, authorLastName) => {
+        return {
+            title: title,
+            author: {
+                firstName: authorFirstName,
+                lastName: authorLastName,
+            },
+        };
+    }
+
+    console.log(createBook("Aesop's Fables", "Aesop"));
+    console.log(createBook("How To Write Gooder Code", "The Angry Developer Behind You", "Watch Out"));
+    console.log(books);
+
+ const showBookInfo = ({books}) => {
+     return
+ }
+
 
 })();
