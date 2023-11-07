7a068cd9d26fb7c753b5239eebdaea9c64cdddd0
mononoke813 <mononoke813@hotmail.com>
2023-11-07 14:44:13 -0600

    fin walkthru

--- js/attributes-script.js
+++ js/attributes-script.js
@@ -1,5 +1,9 @@
+//BONUS
+// const colors = ["red", "orange", "yellow", "green", "blue", "purple", "pink"];
+
+
 (() => {
-// Two seconds after the page loads, change the "profile-pic" src attribute to a different image.
+
     const timeoutImg = setTimeout(() => {
         profileImg = document.querySelector("#profile-pic");
         profileImg.setAttribute("src", "./img/pexels-karim-tab-18624392.jpeg");
@@ -7,7 +11,6 @@
     }, 2000);
 
 
-// Four seconds after the page loads, use innerHTML to change the name in "profile-name".
     const timeoutName = setTimeout(() => {
         const nameChange = document.querySelector("#profile-name");
         nameChange.innerHTML = "Aww Kitteh Kitteh!";
@@ -15,20 +18,28 @@
     }, 4000);
 
 
-// Six seconds after page loads, add a new class to "profile-desc" that changes the color and font of the description text.
     const timeoutColorAndFont = setTimeout(() => {
         const colorFontChange = document.querySelector
         ("#profile-desc");
         colorFontChange.classList.add("profile-desc");
     }, 6000);
 
-// Use setTimout to create these behaviors.
 
-
-// Write code that toggles a class on the "profile-card" that changes its background color every two seconds. Use setInterval.
     const backgroundColor = setInterval(() => {
         const profileBackground = document.querySelector("#profile-card");
-        profileBackground.classList.add("profile-card");
+        profileBackground.classList.toggle("profile-card");
     }, 2000);
 
+    // const backgroundColor = setInterval(() => {
+    //     const profileColor = document.querySelector("#profile-card");
+    //      setInterval(() => {
+    //          const randomIndex = Math.floor(Math.random() * colors.length);
+    //          profileCard.style.backgroundColor = colors[randomIndex];
+    //      }, 2000);
+    //     profileCard.classList.toggle("profile-card");
+    // }, 2000);
+
+
 })();
+
+
